31de2ecae67ebe700ea3e2460d9d13e733f828e2
Valentin <valentin.retter9@etu.univ-lorraine.fr>
2020-10-21 09:40:22 +0200

    Modification de la taille de la fenetre

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -12,7 +12,7 @@ public class Main extends Application {
     public void start(Stage primaryStage) throws Exception{
         Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
         primaryStage.setTitle("Taquin");
-        Scene scene = new Scene(root, 300, 275);
+        Scene scene = new Scene(root, 600, 600);
         primaryStage.setScene(scene);
         primaryStage.show();
     }
