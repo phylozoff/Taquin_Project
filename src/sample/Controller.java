08a0fcfef2bd71d4d74c6945b7dcea9f0b648ee9
Valentin <valentin.retter9@etu.univ-lorraine.fr>
2020-11-04 16:52:19 +0100

    begin play action

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -1,6 +1,11 @@
 package sample;
 
 import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.ToggleButton;
+import javafx.scene.effect.Bloom;
 import javafx.scene.layout.VBox;
 import javafx.scene.text.Text;
 import javafx.scene.text.TextFlow;
@@ -10,11 +15,13 @@ import javafx.stage.Stage;
 import javafx.scene.Parent;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
-
 import java.io.IOException;
 
 public class Controller{
 
+    @FXML
+    private Label label;
+    private ToggleButton play;
 
    public void sayHelp(ActionEvent actionEvent) {
         Stage popUpStage = new Stage();
@@ -36,11 +43,23 @@ public class Controller{
 
     public void hyperLink(ActionEvent actionEvent) {
         Stage primaryStage = new Stage();
+        primaryStage.setTitle("What's Taquin ?");
         WebView browser = new WebView();
         browser.getEngine().load("https://en.wikipedia.org/wiki/15_puzzle");
         VBox vBox = new VBox(browser);
         Scene scene = new Scene(vBox, 960, 600);
         primaryStage.setScene(scene);
         primaryStage.show();
+    }
+
+    @FXML
+    public void playTaquin(ActionEvent actionEvent) {
+        if(play.isSelected()){
+
+        }else{
+
+        }
+
+
     }
 }
--- src/sample/Main.java
+++ src/sample/Main.java
@@ -1,33 +1,38 @@
 package sample;
 
+import javafx.animation.Timeline;
 import javafx.application.Application;
+import javafx.beans.property.DoubleProperty;
+import javafx.beans.property.SimpleDoubleProperty;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.scene.layout.VBox;
+import javafx.scene.paint.Color;
 import javafx.scene.text.Text;
 import javafx.scene.text.TextFlow;
 import javafx.stage.Stage;
+import javafx.util.Duration;
 
 public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception{
+
         FXMLLoader loader = new FXMLLoader(getClass().getResource("sample.fxml"));
         Parent root = loader.load();
         primaryStage.setTitle("Taquin");
-        Text s = new Text("shots");
         Text n = new Text("0");
-        Text c = new Text("chronomètre");
         Text p = new Text("ValouDu57");
+        Text pseudo = new Text("User : ");
         Scene scene = new Scene(root, 600, 400);
-        TextFlow txt = (TextFlow) scene.lookup("#textshot");
-        txt.getChildren().add(s);
-        TextFlow txt1 = (TextFlow) scene.lookup("#shots");
-        txt1.getChildren().add(n);
-        TextFlow txt2 = (TextFlow) scene.lookup("#chrono");
-        txt2.getChildren().add(c);
         TextFlow txt3 = (TextFlow) scene.lookup("#pseudo");
-        txt3.getChildren().add(p);
+        txt3.getChildren().addAll(pseudo,p);
+
+        Label timeLabel = (Label)scene.lookup("#label");
+        timeLabel.setText("0");
+
         primaryStage.setScene(scene);
         primaryStage.show();
     }
